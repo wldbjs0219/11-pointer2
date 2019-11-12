eaf32932e5b8325941d2e911b6ea9c04ff7f86c3
wldbjs0219 <55428633+wldbjs0219@users.noreply.github.com>
2019-11-12 15:47:22 +0900

    11-3

--- main.c
+++ main.c
@@ -5,14 +5,18 @@
 
 int main(void) {
 	
-	int i=10;
-	int *ptr=&i;
+	char buffer[8];
+	double *pd;
+	int *pi;
 	
-	printf("i = %d, ptr = %p\n", i ,ptr);
-	(*ptr)++;
-	printf("i = %d, ptr = %p\n", i, ptr);
-	*ptr++;
-	printf("i = %d, ptr = %p\n", i, ptr);
+	pd=(double*)buffer;
+	*pd=3.14;
+	printf("%f\n",*(double*)buffer);
 	
+	pi=(int*)buffer;
+	*pi=123;
+	*(pi+1)=456;
+	printf("%d %d\n",*(int*)buffer,*((int*)buffer+1));
+
 	return 0;
 }
